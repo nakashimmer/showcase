350f61508ccb52166fcf743a9aa1e6b85f9a9beb
nakashimmer <nakacma@gmail.com>
2018-12-07 00:06:18 +0900

    up

--- script1.js
+++
@@ -1,8 +0,0 @@
-function item(url,title,comment){
-	let str = 
-		'<li onclick=link("'+url+'","'+encodeURI(title)+'","'+comment+'")>'+
-		'<a href="'+'?page='+url+'&title='+title+'" class="dummy">'+title+'</a>'+
-		'</li>';
-		
-	document.write(str);
-}
