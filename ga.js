59fb9bf624bb9ed276ef06351736f2f00296d7e7
nakashimmer <nakacma@gmail.com>
2019-03-20 20:27:02 +0900

    google analytics 設置

---
+++ ga.js
@@ -0,0 +1,12 @@
+//google analytics
+document.write(
+	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-97053215-1"></script>'
+);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+
+gtag('js', new Date()); gtag('config', 'UA-97053215-1');
