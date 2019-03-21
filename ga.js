4c5aab9440a97015d7cd4099a3d2c2eb2742c49e
nakashimmer <nakacma@gmail.com>
2019-03-21 20:27:01 +0900

    up

--- ga.js
+++ ga.js
@@ -1,6 +1,6 @@
 //google analytics
 document.write(
-	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-97053215-1"></script>'
+	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136606141-1"></script>'
 );
 
 window.dataLayer = window.dataLayer || [];
@@ -9,4 +9,4 @@ function gtag() {
 	dataLayer.push(arguments);
 }
 
-gtag('js', new Date()); gtag('config', 'UA-97053215-1');
+gtag('js', new Date()); gtag('config', 'UA-136606141-1');
