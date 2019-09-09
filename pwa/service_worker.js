1c040fa8c2500d18307d2ea9647df828ac41ebeb
nakashimmer <nakacma@gmail.com>
2019-09-10 00:48:52 +0900

    up

--- pwa/service_worker.js
+++ pwa/service_worker.js
@@ -1,21 +1,18 @@
-// キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
 	'/poster-keisuke.github.io/',
 ];
-
-// インストール処理
 self.addEventListener('install', function (event) {
+	// Perform install steps
 	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
+		caches.open(CACHE_NAME)
 			.then(function (cache) {
-				return cache.addAll(urlsToCache);
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache.map(url => new Request(url, { credentials: 'same-origin' })));
 			})
 	);
 });
 
-// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function (event) {
 	event.respondWith(
 		caches
