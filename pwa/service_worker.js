da5119328a36aea3a74cdc0fd441f5380e03b1a6
nakashimmer <3724063+nakashimmer@users.noreply.github.com>
2019-09-21 12:16:35 +0900

    up

--- pwa/service_worker.js
+++ pwa/service_worker.js
@@ -1,9 +1,9 @@
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-	'/',
-	'./index.html',
-	'./icon.png',
-	'./sound.mp3'
+	'/showcase/pwa/',
+	'/showcase/pwa/index.html',
+	'/showcase/pwa/icon.png',
+	'/showcase/pwa/sound.mp3'
 ];
 
 // インストール処理
